63eeb9a49e3be1ab0ff20f006b8d3d1eaae71d82
Vic <victor.rodriguez02@student.csulb.edu>
2020-04-29 14:40:31 -0700

    Initial commit. Wanted to see if I could generate a prefab with a random color. I absolutely can.

---
+++ GeneratePipes.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePipes : MonoBehaviour{
+
+    public GameObject pipePrefab;
+
+    // Start is called before the first frame update
+    void Start(){
+        GameObject initalPipe = Instantiate(pipePrefab,new Vector3(0,0,0), Quaternion.identity);
+        initalPipe.GetComponent<MeshRenderer>().material.color = new Color(Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
+    }
+
+    // Update is called once per frame
+    void Update(){
+        
+
+    }
+}
