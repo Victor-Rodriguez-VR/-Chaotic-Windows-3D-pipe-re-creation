1c4b6cb712670fab84701fecebb1765eb1b7719c
Vic <victor.rodriguez02@student.csulb.edu>
2020-05-17 15:54:11 -0700

    Massive streamlining. Make the # of lines drastically go down. Similarly, increased speed by using arrays. Will update on pipe detection. This project is in its final stages

--- GeneratePipes.cs
+++ GeneratePipes.cs
@@ -6,11 +6,21 @@ public class GeneratePipes : MonoBehaviour{
 
     public GameObject pipePrefab = null; // Our prefab in the Unity Editor's assets. Resembles a cylinder.
     public GameObject spherePrefab = null; // Our prefab in the Unity Editor's assets. Resembles a sphere.
+    string[] variables = {"x","-x","z","-z", "y","-y",};
+    private static Quaternion[] variableRotations={
+        Quaternion.Euler(0f,0f,-90f),
+        Quaternion.Euler(0f,0f,90f),
+        Quaternion.Euler(90f,0f,0f),
+        Quaternion.Euler(-90f,0f,0f),
+		Quaternion.Euler(0f,180f,0f), 
+        Quaternion.Euler(180f,0f,0f)
+    };
+
 
 
     // Start is called before the first frame update
     void Start(){
-        continuePipe(spawnAPrefabSomewhere(), "y");      
+        continuePipe(spawnAPrefabSomewhere(), 5);      
     }
 
 
@@ -20,37 +30,27 @@ public class GeneratePipes : MonoBehaviour{
     }
 
     public GameObject spawnAPrefabSomewhere(){
-        Vector3 spawnLocation = new Vector3(Random.Range(-10.0f, 10.0f), Random.Range(-4.5f, 5.0f) , 0);
+        Vector3 spawnLocation = new Vector3(Random.Range(-5.0f, 5.0f), Random.Range(-4.5f, 5.0f) , 0);
         GameObject objec = Instantiate(pipePrefab, spawnLocation, Quaternion.Euler(0f,180f,0f));
         objec.GetComponent<Renderer>().material.color = new Color(Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f), Random.Range (0f, 1f));
         return objec;
     }
 
-    public void continuePipe(GameObject previousPipe, string previousDirection){ 
+    public void continuePipe(GameObject previousPipe, int previousDirection){ 
         if(outOfBounds(previousPipe.transform)){
             return;
         }
-        string direction = randomTransform(previousDirection);
+        int direction = randomTransform(previousDirection);
         Color parentColor = previousPipe.GetComponent<Renderer>().material.color;
-        if(changesDirection()){
-            if(previousDirection == direction ){
-                GameObject piper = Instantiate(pipePrefab,previousPipe.transform.position + previousPipe.transform.up, Quaternion.Euler(previousPipe.transform.eulerAngles.x, previousPipe.transform.eulerAngles.y, previousPipe.transform.eulerAngles.z));
-                piper.transform.SetParent(previousPipe.transform, true);
-                piper.GetComponent<Renderer>().material.color = parentColor;
-                continuePipe(piper, previousDirection);
-            }
-            else{
+        if(changesDirection() && previousDirection != direction){ 
                 GameObject spherey = Instantiate(spherePrefab, previousPipe.transform.position + previousPipe.transform.up, Quaternion.Euler(0f, 0f, 0f));
                 spherey.transform.SetParent(previousPipe.transform, true);
                 spherey.GetComponent<Renderer>().material.color = parentColor;
-                if(alreadyFilled(spherey, newDirection(spherey, direction))){
-                    direction = randomTransform(previousDirection);
-                }
                 GameObject piper = Instantiate(pipePrefab, spherey.transform.position + newDirection(spherey, direction), rotation(direction));
-                piper.transform.SetParent(spherey.transform,true);
+                piper.transform.SetParent(spherey.transform, true);
                 piper.GetComponent<Renderer>().material.color = parentColor;
                 continuePipe(piper, direction);
-            }
+
         }
         else{
             GameObject piper = Instantiate(pipePrefab,previousPipe.transform.position + previousPipe.transform.up, Quaternion.Euler(previousPipe.transform.eulerAngles.x, previousPipe.transform.eulerAngles.y, previousPipe.transform.eulerAngles.z));
@@ -65,47 +65,16 @@ public class GeneratePipes : MonoBehaviour{
 
         @param previousDirection - 
     */
-    public string randomTransform(string previousDirection){
-        int randomNumber = Random.Range(0,198);
-        string transformAxis = "";
-        if(randomNumber < 33 ){
-            transformAxis =  "x";
-            if(previousDirection == "-x"){
-                return randomTransform(previousDirection);
-            }
+    public int randomTransform(int lastIndex){
+        int idk = Random.Range(0,6);
+        int modMe = 1;
+        if(lastIndex %2 == 1){
+            modMe = -1;
         }
-        else if(randomNumber < 66){
-            transformAxis = "-x";
-            if(previousDirection == "x"){
-                return randomTransform(previousDirection);
-            }
+        if( (lastIndex+modMe) != idk){
+            return idk;
         }
-        else if(randomNumber <99 ){
-            transformAxis = "y";
-            if(previousDirection == "-y"){
-                return randomTransform(previousDirection);
-            }
-        }
-        else if(randomNumber < 132){
-            transformAxis = "-y";
-            if(previousDirection == "y"){
-                return randomTransform(previousDirection);
-            }
-        }
-        else if(randomNumber < 165){
-            transformAxis = "z";
-            if(previousDirection == "-z"){
-                return randomTransform(previousDirection);
-            }
-        }
-        else{
-            transformAxis = "-z";
-            if(previousDirection == "z"){
-                return randomTransform(previousDirection);
-            }
-        }
-        return transformAxis;
-        
+        return randomTransform(lastIndex);
     }
 
     /*
@@ -115,75 +84,35 @@ public class GeneratePipes : MonoBehaviour{
 
         @return a Vector3 that determines in which direction the pipe will go.
     */
-    public Vector3 newDirection(GameObject previousObject, string randomTransform){
-        if(randomTransform == "x"){
-            return previousObject.transform.right;
+    public Vector3 newDirection(GameObject previousObject, int randomTransform){
+        int rotation180 = 1;
+        if(randomTransform % 2 == 1 ){
+            rotation180 = -1;
         }
-        else if (randomTransform == "-x"){
-            return previousObject.transform.right *-1;
+        if(randomTransform < 2){
+            return previousObject.transform.right * rotation180;
         }
-        else if (randomTransform == "y"){
-            return previousObject.transform.up;
-        }
-        else if (randomTransform == "-y"){
-            return previousObject.transform.up*-1; 
-        }
-        else if (randomTransform == "-z"){
-            return previousObject.transform.forward *-1;
+        else if (randomTransform < 4 ){
+            return previousObject.transform.forward* rotation180; 
         }
         else{
-            return previousObject.transform.forward;
+            return previousObject.transform.up * rotation180;
         }
 
     }
    
-
-   // Make a function that takes in a Vector3 (newDirection) and returns a new Quaternion / orientation. 
-   // You'd use this in ContinuePipe right after making a new Sphere you'd want to know where it went. Based off of where it went it'd be
-   // easy to rotate to make the parts connect.
-
-
     /*
         Returns eulerAngles (x,y,z) in Quaternion form based on axis of rotation.
 
-
         @param rotationAxis - the desired axis of rotation (ex: from our randomTransform we got x so we are going to rotate around x).
     */
-   public Quaternion rotation(string rotationAxis){
-       // These rotations are nice and all, but they're somewhat limited.
-       // Our spheres spawn nicely. But where's the fun in this??
-       // Next I will make it so the engine rolls to do different rotations in x and y not just limited to 90 degree rotations.
-       if(rotationAxis == "x"){
-            Debug.Log("x");
-           return Quaternion.Euler(0f,0f,-90f);
-       }
-       if(rotationAxis == "-x"){
-           Debug.Log("-x");
-           return Quaternion.Euler(0f,0f,90f);
-       }
-       if(rotationAxis == "z"){
-           Debug.Log("z");
-           return Quaternion.Euler(90f,0f,0f);
-       }
-       if( rotationAxis == "-z"){
-           Debug.Log("-z");
-           return Quaternion.Euler(-90f,0f,0f);
-       }
-       else if (rotationAxis == "y"){
-           Debug.Log("y");
-           return Quaternion.Euler(0f,180f,0f);
-       }
-       else if (rotationAxis == "-y"){
-           Debug.Log("-y");
-           return Quaternion.Euler(180f,0f,0f);
-       }
-       Debug.Log("I go here");
-       return Quaternion.Euler(0f,0f,0f);
+   public Quaternion rotation(int variable){
+       return variableRotations[variable];
    }
 
    public bool changesDirection(){
        int randomNumber = Random.Range(0,1000);
-       if(randomNumber > 601){
+       if(randomNumber > 400){
            return true;
        }
        return false;
