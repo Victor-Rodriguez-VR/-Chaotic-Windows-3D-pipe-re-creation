a3c3ae67fc054ffae56fe3bd45cddfd3310b07da
Vic <victor.rodriguez02@student.csulb.edu>
2020-07-24 20:10:53 -0700

    Minor commits!

--- PipePooler.cs
+++ PipePooler.cs
@@ -22,7 +22,7 @@ public class PipePooler : MonoBehaviour {
 
   void Start () {
     pipeAndSpherePool = new List<GameObject>();
-    Vector3 newCameraPosition = new Vector3(Random.Range(-20.0f, -15.0f), Random.Range(-10.0f, -5.0f) , Random.Range(-13.0f, -3.0f));
+    Vector3 newCameraPosition = new Vector3(Random.Range(-20.0f, -15.0f), Random.Range(-10.0f, 5.0f) , Random.Range(-13.0f, -3.0f));
     gameCamera.transform.position = newCameraPosition;
     gameCamera.transform.LookAt( new Vector3(0f,0f,0f ));
     foreach (ObjectPoolItem item in objectsToBePooled) {
@@ -57,16 +57,25 @@ public class PipePooler : MonoBehaviour {
     return null;
   }
 
+  /*
+    * Deactivates an object within pipeAndSpherePool.
+    * @param poolIndex - the index of the object we are trying to delete from 
+  */
   public void RemovePooledObject(int poolIndex){
       pipeAndSpherePool[poolIndex].SetActive(false);
   }
 
+  /*
+    * Retrieves the index of the most available object.
+
+    * @param tag - the tag of the most available object. 
+  */
   public int getPoolIndex(string tag){
     for (int i = 0; i < pipeAndSpherePool.Count; i++) {
       if (!pipeAndSpherePool[i].activeInHierarchy && pipeAndSpherePool[i].tag == tag) {
         return i;
       }
     }
-    return -50;
+    return -50; // some random value.
   }
 }
