ffb683c8252a3e41968e737bc86e9e7958a5928b
Victor Rodriguez <47678223+Victor-Rodriguez-VR@users.noreply.github.com>
2020-07-31 18:04:40 -0700

    Update PipePooler.cs
    One of the final commits for PipePooler.

--- PipePooler.cs
+++ PipePooler.cs
@@ -1,11 +1,11 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-// This class acts like a struct - all variables are public. A struct may will help with performance. 
+// This class acts like a struct - all variables are public. 
 [System.Serializable]
 public class ObjectPoolItem {
-  public GameObject objectToPool; // The specific GameObject we are inserting into the objectPool.
+  public GameObject objectToPool; // The prefab we are making a gameobject.
   public int amountToPool;  // The number of objects we instantiate upon start.
   public bool shouldExpand; // Determines whether more Gameobjects should be added to the pool.
 }
@@ -13,30 +13,28 @@ public class ObjectPoolItem {
 public class PipePooler : MonoBehaviour {
 
 	public static PipePooler SharedInstance; // To instantiate as few objects as possible, all GeneratePipe files running in parallel will share objs.
-  public List<ObjectPoolItem> objectsToBePooled; //  List of all Object types we need to pool. Instantiated in Unity's editor.
-  public List<GameObject> pipeAndSpherePool;  // The actual list of all Uninstantiated GameObjects.
+  public List<ObjectPoolItem> objectsToBePooled; //  List of all Object we plan to pool.
+  public List<GameObject> ObjectPool;  // The list of all active and inactive prefabs. 
   public Camera gameCamera; // The main camera in the game world.
   public  AudioSource song; // can be any song :)
+
 	void Awake() {
 	  SharedInstance = this; 
-	}
-
-  void Start () {
-    song =  GetComponent<AudioSource>();
-    song.time = 73.0f;
-    pipeAndSpherePool = new List<GameObject>();
     Vector3 newCameraPosition = new Vector3(Random.Range(-20.0f, -15.0f), Random.Range(-10.0f, 5.0f) , Random.Range(-13.0f, -3.0f));
     gameCamera.transform.position = newCameraPosition;
     gameCamera.transform.LookAt( new Vector3(0f,0f,0f ));
+    song =  GetComponent<AudioSource>();
+    song.time = 73.0f;
+    ObjectPool = new List<GameObject>();
     foreach (ObjectPoolItem item in objectsToBePooled) {
       for (int i = 0; i < item.amountToPool; i++) {
         GameObject obj = (GameObject)Instantiate(item.objectToPool);
         obj.SetActive(false);
-        pipeAndSpherePool.Add(obj);
+        ObjectPool.Add(obj);
       }
     }
   }
-  // Future plans: make the application go along with the beat and move the camera to different locations for extra depth. Because it sounds cool! :) Or not depends.
+
 	
   /*
     * Gets the most readibly accessible Pipe object that is instantiated but inactive.
@@ -44,9 +42,8 @@ public class PipePooler : MonoBehaviour {
     * @return - If an inactive GameObject shares the same tag, return said GameObject. Otherwise would return null.
   */
   public GameObject GetPooledObject(string tag, int index) {
-    Debug.Log(song.time);
     if (index != -50) {
-      return pipeAndSpherePool[index];
+      return ObjectPool[index];
     }
     
     foreach (ObjectPoolItem item in objectsToBePooled) { // Checks our objectsToBePooled to confirm whether or not we can instantiate more.
@@ -54,7 +51,7 @@ public class PipePooler : MonoBehaviour {
             if (item.shouldExpand) {
             GameObject obj = (GameObject)Instantiate(item.objectToPool);
             obj.SetActive(false);
-            pipeAndSpherePool.Add(obj);
+            ObjectPool.Add(obj);
             return obj;
             }
         }
@@ -63,24 +60,25 @@ public class PipePooler : MonoBehaviour {
   }
 
   /*
-    * Deactivates an object within pipeAndSpherePool.
+    * Deactivates an object within ObjectPool.
     * @param poolIndex - the index of the object we are trying to delete from 
   */
   public void RemovePooledObject(int poolIndex){
-      pipeAndSpherePool[poolIndex].SetActive(false);
+      ObjectPool[poolIndex].SetActive(false);
   }
 
   /*
     * Retrieves the index of the most available object.
 
-    * @param tag - the tag of the most available object. 
+    * @param tag - the tag of the object type we want. 
   */
   public int getPoolIndex(string tag){
-    for (int i = 0; i < pipeAndSpherePool.Count; i++) {
-      if (!pipeAndSpherePool[i].activeInHierarchy && pipeAndSpherePool[i].tag == tag) {
+    for (int i = 0; i < ObjectPool.Count; i++) {
+      if (!ObjectPool[i].activeInHierarchy && ObjectPool[i].tag == tag) {
         return i;
       }
     }
-    return -50; // some random value.
+    return -50; // some random value that denotes no index found.
   }
 }
+
